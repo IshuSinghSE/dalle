b9242115c4374be279edbf65941408e1307a9d74
Ashu Singh <ishu.111636@gmail.com>
2023-02-01 23:43:26 +0530

    server deployed and updated

--- server/index.js
+++ server/index.js
@@ -24,7 +24,9 @@ app.get('/', async (req, res) => {
 const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
-    app.listen(8080, () => console.log('Server has started on port http://localhost:8080'))
+    app.listen(8080, () =>
+      console.log("Server has started on port https://dalle-jh74.onrender.com")
+    );
   } catch (error) {
     console.log(error);
   }

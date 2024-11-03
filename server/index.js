e6db5544ed70cad69dfa68ac2b108e3bba8cb3cb
Ishu Singh <ishu.111636@yahoo.com>
2024-11-04 00:57:58 +0530

    allowed cors

--- server/index.js
+++ server/index.js
@@ -9,7 +9,9 @@ import dalleRoutes from './routes/dalleRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://dalle3.vercel.app'
+}));
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1/post', postRoutes);

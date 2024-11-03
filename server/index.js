a2d770c203d5b94374f0659cce06f9e7e3315584
Ishu Singh <ishu.111636@yahoo.com>
2024-11-04 01:10:21 +0530

    updated cors

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: 'https://dalle3.vercel.app',
+  origin: ['https://dalle3.vercel.app', 'https://dalle-server.netlify.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
